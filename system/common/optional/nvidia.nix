ba2c1cbbb863ea99383a295e4ce965e2a5044108
Parthiv Krishna <parthiv@midnight.sub0.net>
2025-03-19 07:42:32 +0000

    nvtop builds but doesn't detect GPU

--- system/common/optional/nvidia.nix
+++ system/common/optional/nvidia.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -17,8 +18,34 @@
   };
 
   unfree.allowedPackages = [
+    "cuda_cccl"
+    "cuda_cudart"
+    "cuda_cuobjdump"
+    "cuda_cupti"
+    "cuda_cuxxfilt"
+    "cuda_gdb"
+    "cuda_nvcc"
+    "cuda_nvdisasm"
+    "cuda_nvml_dev"
+    "cuda_nvrtc"
+    "cuda_nvtx"
+    "cuda-merged"
+    "cuda_nvprune"
+    "cuda_profiler_api"
+    "cuda_sanitizer_api"
+    "libcublas"
+    "libcufft"
+    "libcurand"
+    "libcusolver"
+    "libcusparse"
+    "libnpp"
+    "libnvjitlink"
     "nvidia-x11"
     "nvidia-settings"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.nvidia
   ];
 
 }
