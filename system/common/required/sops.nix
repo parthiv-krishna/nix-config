b8d93cc75f9fcfdbe74d168c1bf9958c4896af9d
Parthiv Krishna <parthiv@midnight.sub0.net>
2025-03-20 06:42:26 +0000

    sops fixes: key gen at boot and env var

--- system/common/required/sops.nix
+++ system/common/required/sops.nix
@@ -14,13 +14,17 @@
 
     age = {
       # if key not present at keyFile, automatically generate from ssh key
-      keyFile = "/var/lib/sops-nix/key.txt";
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       generateKey = true;
+      keyFile = "/var/lib/sops-nix/key.txt";
+      # need to point to /persist as persistence setup to /etc/ssh may not be
+      # ready when secrets-for-users are computed.
+      # https://github.com/Mic92/sops-nix/commit/4c4fb93f18b9072c6fa1986221f9a3d7bf1fe4b6
+      sshKeyPaths = [ "/persist/system/etc/ssh/ssh_host_ed25519_key" ];
     };
 
     secrets = {
       parthiv-password = { };
     };
   };
+
 }
