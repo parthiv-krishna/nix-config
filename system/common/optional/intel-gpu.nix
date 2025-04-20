0fc4dd8ea15985003e174243d0e0b30e025654db
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-04-20 10:11:35 +0000

    immich w/ quicksync transcode + CUDA ML

---
+++ system/common/optional/intel-gpu.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+
+  services.xserver.videoDrivers = [ "intel" ];
+
+  hardware.opengl = {
+    enable = lib.mkDefault true;
+
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+      vaapiVdpau
+      libvdpau-va-gl
+      vaapiIntel
+      intel-ocl
+      vpl-gpu-rt
+    ];
+  };
+
+  unfree.allowedPackages = [
+    "intel-ocl"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    intel-gpu-tools
+    nvtopPackages.intel
+  ];
+
+}
