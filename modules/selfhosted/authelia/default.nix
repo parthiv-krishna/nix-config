8b8029d788c5fdad723e11958530d16bd65c31d9
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-05-21 09:22:56 +0000

    authelia secret injection

--- modules/selfhosted/authelia/default.nix
+++ modules/selfhosted/authelia/default.nix
@@ -14,146 +14,62 @@ let
     in
     {
       sops.secrets.${fullSecretPath} = { };
+      # inject secret into authelia configuration
       services.authelia.instances.${instance}.settings = lib.setAttrByPath attrPath secretTemplate;
     };
-in
-{
 
-}
-// lib.custom.mkSelfHostedService {
+  originalAutheliaConfig =
+    builtins.foldl' lib.recursiveUpdate
+      {
+        services.authelia.instances.${instance} = {
+          enable = true;
+          secrets.manual = true;
+          environmentVariables = {
+            # enable templating filter for secrets
+            X_AUTHELIA_CONFIG_FILTERS = "template";
+          };
+          # initial settings before secret injection
+          settings = import ./settings.nix { };
+        };
+      }
+      [
+        # secrets to be injected into authelia configuration
+        (mkSecret "identity_validation/reset_password/jwt_secret")
+        (mkSecret "session/secret")
+        (mkSecret "session/redis/password")
+        (mkSecret "storage/encryption_key")
+        (mkSecret "identity_providers/oidc/hmac_secret")
+        (mkSecret "identity_providers/oidc/jwks/main/key")
+        (mkSecret "identity_providers/oidc/clients/actual/client_id")
+        (mkSecret "identity_providers/oidc/clients/actual/client_secret")
+        (mkSecret "identity_providers/oidc/clients/immich/client_id")
+        (mkSecret "identity_providers/oidc/clients/immich/client_secret")
+        (mkSecret "identity_providers/oidc/clients/jellyfin/client_id")
+        (mkSecret "identity_providers/oidc/clients/jellyfin/client_secret")
+        # Persistent directory configuration
+        (lib.custom.mkPersistentSystemDir { directory = "/var/lib/private/authelia"; })
+      ];
+
+  # convert oidc jwks/clients from attribute sets to lists of values
+  originalAutheliaSettings = originalAutheliaConfig.services.authelia.instances.${instance}.settings;
+  originalOidcSettings = originalAutheliaSettings.identity_providers.oidc;
+  transformedOidcSettings = lib.recursiveUpdate originalOidcSettings {
+    jwks = lib.attrValues originalOidcSettings.jwks;
+    clients = lib.attrValues originalOidcSettings.clients;
+  };
+  transformedAutheliaSettings = lib.recursiveUpdate originalAutheliaSettings {
+    identity_providers = lib.recursiveUpdate originalAutheliaSettings.identity_providers {
+      oidc = transformedOidcSettings;
+    };
+  };
+  transformedAutheliaConfig = lib.recursiveUpdate originalAutheliaConfig {
+    services.authelia.instances.${instance}.settings = transformedAutheliaSettings;
+  };
+in
+lib.custom.mkSelfHostedService {
   inherit config lib;
   name = "authelia";
   hostName = "vardar";
-  subdomain = "auth";
   public = true;
-  serviceConfig = lib.mkMerge [
-    {
-      services.authelia.instances.${instance} = {
-        enable = true;
-        secrets.manual = true;
-        environmentVariables = {
-          # Enable templating filter for secrets
-          X_AUTHELIA_CONFIG_FILTERS = "template";
-        };
-        settings = lib.mkMerge [
-          {
-            server.address = "tcp://:9091";
-            theme = "dark";
-            log = {
-              level = "warn";
-              format = "text";
-              file_path = "/log/authelia.log";
-            };
-            totp.issuer = "sub0.net";
-            authentication_backend.file.path = "/data/users_database.yml";
-            access_control = {
-              default_policy = "deny";
-              rules = [
-                {
-                  domain_regex = "[a-z0-9]*.sub0.net";
-                  policy = "bypass";
-                }
-              ];
-            };
-            session = {
-              cookies = [
-                {
-                  name = "sub0_session";
-                  domain = "sub0.net";
-                  authelia_url = "https://auth.sub0.net";
-                  expiration = "1 hour";
-                  inactivity = "5 minutes";
-                }
-              ];
-              redis = {
-                host = "redis";
-                port = 6379;
-              };
-            };
-            regulation = {
-              max_retries = 3;
-              find_time = "2 minutes";
-              ban_time = "5 minutes";
-            };
-            storage = {
-              local.path = "/data/db.sqlite3";
-            };
-            # TODO: setup SMTP server for email
-            notifier = {
-              disable_startup_check = false;
-              filesystem.filename = "/data/notification.txt";
-            };
-            identity_providers.oidc = {
-              jwks = {
-                main = {
-                  algorithm = "RS256";
-                  use = "sig";
-                };
-              };
-              clients = {
-                actual = {
-                  client_name = "Actual";
-                  public = false;
-                  authorization_policy = "one_factor";
-                  redirect_uris = [ "https://actual.sub0.net/openid/callback" ];
-                  scopes = [
-                    "email"
-                    "groups"
-                    "openid"
-                    "profile"
-                  ];
-                  userinfo_signed_response_alg = "none";
-                  token_endpoint_auth_method = "client_secret_basic";
-                };
-                immich = {
-                  client_name = "Immich";
-                  public = false;
-                  authorization_policy = "one_factor";
-                  redirect_uris = [
-                    "https://immich.sub0.net/auth/login"
-                    "https://immich.sub0.net/user-settings"
-                    "app.immich:///oauth-callback"
-                  ];
-                  scopes = [
-                    "openid"
-                    "profile"
-                    "email"
-                  ];
-                  userinfo_signed_response_alg = "none";
-                };
-                jellyfin = {
-                  client_name = "Jellyfin";
-                  public = false;
-                  authorization_policy = "one_factor";
-                  require_pkce = true;
-                  redirect_uris = [ "https://jellyfin.sub0.net/sso/OID/redirect/authelia" ];
-                  scopes = [
-                    "groups"
-                    "openid"
-                    "profile"
-                  ];
-                  userinfo_signed_response_alg = "none";
-                  token_endpoint_auth_method = "client_secret_post";
-                };
-              };
-            };
-          }
-        ];
-      };
-    }
-    # Secret declarations and injections
-    (mkSecret "identity_validation/reset_password/jwt_secret")
-    (mkSecret "session/secret")
-    (mkSecret "session/redis/password")
-    (mkSecret "storage/encryption_key")
-    (mkSecret "identity_providers/oidc/hmac_secret")
-    (mkSecret "identity_providers/oidc/jwks/main/key")
-    (mkSecret "identity_providers/oidc/clients/actual/client_id")
-    (mkSecret "identity_providers/oidc/clients/actual/client_secret")
-    (mkSecret "identity_providers/oidc/clients/immich/client_id")
-    (mkSecret "identity_providers/oidc/clients/immich/client_secret")
-    (mkSecret "identity_providers/oidc/clients/jellyfin/client_id")
-    (mkSecret "identity_providers/oidc/clients/jellyfin/client_secret")
-    (lib.custom.mkPersistentSystemDir { directory = "/var/lib/private/authelia"; })
-  ];
+  serviceConfig = transformedAutheliaConfig;
 }
