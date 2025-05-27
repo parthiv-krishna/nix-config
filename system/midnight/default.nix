222578263f2d9da8eddf68d3627f6ae111ec041a
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-05-27 01:53:57 -0700

    back to centralized drive configs

--- system/midnight/default.nix
+++ system/midnight/default.nix
@@ -11,7 +11,10 @@
     ./hardware-configuration.nix
 
     # disks
-    ./disks.nix
+    (import (lib.custom.relativeToRoot "system/common/disks/boot_drive_external_nix.nix") {
+      device = "/dev/disk/by-id/ata-ADATA_SP610_1F1220031635";
+      swapSize = "8G";
+    })
 
     # users
     (map (lib.custom.relativeTo "system/common/users/") [
@@ -39,44 +42,6 @@
     # Use the systemd-boot EFI boot loader.
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    # suppress raid warnings
-
-    # raid setup
-    swraid.mdadmConf = ''
-      MAILADDR nobody@nowhere
-    '';
-  };
-
-  # setup cached device on boot
-  systemd.services.setup-dmcache = {
-    description = "Setup dmcache for /data";
-    wantedBy = [ "local-fs-pre.target" ];
-    before = [ "local-fs-pre.target" ];
-    after = [
-      "lvm2-activation.service"
-      "systemd-cryptsetup@crypted_array0.service"
-    ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      modprobe dm-cache dm-cache-smq
-      udevadm settle
-      if [ ! -e /dev/mapper/cached_data ]; then
-        CACHE_DATA_SIZE=$(blockdev --getsz /dev/cache_vg/cache_data)
-        DATA_SIZE=$(blockdev --getsz /dev/data_vg/data)
-        DATA_BLOCK_SIZE=256
-        dmsetup create cached_data --table "0 $DATA_SIZE cache /dev/cache_vg/cache_meta /dev/cache_vg/cache_data /dev/data_vg/data $DATA_BLOCK_SIZE 1 writeback default 0"
-      fi
-    '';
-  };
-
-  # Filesystem mount
-  fileSystems."/data" = {
-    device = "/dev/mapper/cached_data";
-    fsType = "btrfs";
-    options = [ "noatime" ];
   };
 
   time.timeZone = "Etc/UTC";
