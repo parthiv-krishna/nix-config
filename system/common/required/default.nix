3a444b21b07673958111b420df6ef6d921991b14
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-06-01 03:10:09 -0700

    cachix

--- system/common/required/default.nix
+++ system/common/required/default.nix
@@ -6,11 +6,20 @@
   imports = lib.custom.scanPaths ./.;
 
   # enable flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
+    # use cachix
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   users.users.root.hashedPassword = "*"; # no root password
 
 }
