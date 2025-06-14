eb2af2655e7d3c2df36e765ec9ef217c72680ad0
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-06-14 11:11:20 -0700

    move important utilities from home to system

--- home/parthiv/common/required/default.nix
+++ home/parthiv/common/required/default.nix
@@ -1,21 +1,7 @@
 {
   lib,
-  pkgs,
   ...
 }:
 {
   imports = lib.custom.scanPaths ./.;
-
-  home.packages = with pkgs; [
-    curl
-    fastfetch
-    htop
-    pciutils
-    powertop
-    trash-cli
-    unzip
-    usbutils
-    wget
-    zip
-  ];
 }
