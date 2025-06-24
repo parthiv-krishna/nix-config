4c80f4f7da96e7376d0aa9337a7e3ddc8071ce57
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-06-24 08:49:02 -0700

    use systemd-resolved

--- system/common/required/tailscale.nix
+++ system/common/required/tailscale.nix
@@ -16,6 +16,16 @@ in
 
   sops.secrets.${secretName} = { };
 
+  # tailscale integrates with systemd-resolved
+  services.resolved = {
+    enable = true;
+    fallbackDns = [
+      # quad9
+      "9.9.9.9"
+      "149.112.112.112"
+    ];
+  };
+
   # persist tailscale state
   environment.persistence."/persist/system" = {
     directories = [
