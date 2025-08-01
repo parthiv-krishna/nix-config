afc17d454020b6cccbc3466144e69c7700004e7d
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-08-01 16:43:24 -0700

    move core packages to home-manager

--- home/common/required/default.nix
+++ home/common/required/default.nix
@@ -1,7 +1,22 @@
 {
   lib,
+  pkgs,
   ...
 }:
 {
   imports = lib.custom.scanPaths ./.;
+
+  # core utils
+  home.packages = with pkgs; [
+    curl
+    fastfetch
+    htop
+    pciutils
+    powertop
+    trash-cli
+    unzip
+    usbutils
+    wget
+    zip
+  ];
 }
