52c4c3094bd2cb23cb6c72eac552e7066c4d3e62
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-08-18 20:12:24 -0700

    ssh key available to root user for auto upgrade

--- system/common/required/auto-upgrade.nix
+++ system/common/required/auto-upgrade.nix
@@ -16,18 +16,20 @@
   };
 
   # garbage collection after auto-upgrade
-  systemd.services.nix-gc-after-upgrade = {
-    description = "Nix garbage collection after auto-upgrade";
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-      ExecStart = "${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 10d";
+  systemd.services = {
+    nix-gc-after-upgrade = {
+      description = "Nix garbage collection after auto-upgrade";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "${pkgs.nix}/bin/nix-collect-garbage --delete-older-than 10d";
+      };
     };
-  };
 
-  # trigger garbage collection after successful upgrade
-  systemd.services.nixos-upgrade = {
-    onSuccess = [ "nix-gc-after-upgrade.service" ];
+    # trigger garbage collection after successful upgrade
+    nixos-upgrade = {
+      onSuccess = [ "nix-gc-after-upgrade.service" ];
+    };
   };
 
   # discord notifications
@@ -35,4 +37,10 @@
     nixos-upgrade.enable = true;
     nix-gc-after-upgrade.enable = true;
   };
+
+  # make ssh key accessible to auto upgrade service
+  systemd.tmpfiles.rules = [
+    "L /root/.ssh/id_ed25519 - - - - ${config.users.users.parthiv.home}/.ssh/id_ed25519"
+  ];
+
 }
