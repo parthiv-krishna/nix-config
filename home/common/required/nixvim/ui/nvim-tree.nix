517b99e7d0ae694ce33f7b39cd3eea2ee9368208
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-08-18 18:55:08 -0700

    fix eval warnings

--- home/common/required/nixvim/ui/nvim-tree.nix
+++ home/common/required/nixvim/ui/nvim-tree.nix
@@ -3,21 +3,23 @@ _: {
     enable = true;
     openOnSetup = true;
     openOnSetupFile = true;
-    autoReloadOnWrite = true;
+    settings = {
+      auto_reload_on_write = true;
 
-    # reclaim Ctrl-K for tmux navigator
-    onAttach = {
-      __raw = ''
-        function(bufnr)
-          local function opts(desc)
-            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      # reclaim Ctrl-K for tmux navigator
+      on_attach = {
+        __raw = ''
+          function(bufnr)
+            local function opts(desc)
+              return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            local api = require("nvim-tree.api")
+            api.config.mappings.default_on_attach(bufnr)
+            vim.keymap.set("n", "<C-K>", ":TmuxNavigateUp<CR>", opts("Refresh"))
           end
-
-          local api = require("nvim-tree.api")
-          api.config.mappings.default_on_attach(bufnr)
-          vim.keymap.set("n", "<C-K>", ":TmuxNavigateUp<CR>", opts("Refresh"))
-        end
-      '';
+        '';
+      };
     };
   };
 
