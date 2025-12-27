44e18c5fc5a715afe01655964c68938685e1e38e
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2025-12-27 12:23:07 -0800

    dig / smartctl

--- home/common/required/default.nix
+++ home/common/required/default.nix
@@ -10,11 +10,13 @@
   home.packages = with pkgs; [
     btop
     curl
+    dig
     fastfetch
     fzf
     pciutils
     powertop
     ripgrep
+    smartmontools
     trash-cli
     unzip
     usbutils
