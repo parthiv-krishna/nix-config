bbd669d7545d1e684128111b8af2a84f02a5df9b
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2026-01-05 17:45:13 -0800

    a bunch of nixvim lsp features

--- home/common/required/nixvim/lsp/default.nix
+++ home/common/required/nixvim/lsp/default.nix
@@ -1,7 +1,112 @@
-{
-  lib,
-  ...
-}:
+{ lib, ... }:
 {
   imports = lib.custom.scanPaths ./.;
+
+  programs.nixvim.plugins.lsp = {
+    enable = true;
+
+    capabilities = ''
+      capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      -- autocomplete capabilities
+      capabilities.textDocument.completion.completionItem = {
+        documentationFormat = { "markdown", "plaintext" },
+        snippetSupport = true,
+        preselectSupport = true,
+        insertReplaceSupport = true,
+        labelDetailsSupport = true,
+        deprecatedSupport = true,
+        commitCharactersSupport = true,
+        tagSupport = { valueSet = { 1 } },
+        resolveSupport = {
+          properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+          },
+        },
+      }
+
+      -- semantic tokens
+      capabilities.textDocument.semanticTokens = {
+        multilineTokenSupport = true,
+        overlappingTokenSupport = true,
+        serverCancelSupport = true,
+        augmentsSyntaxTokens = true,
+      }
+
+      -- code action support
+      capabilities.textDocument.codeAction = {
+        dynamicRegistration = false,
+        codeActionLiteralSupport = {
+          codeActionKind = {
+            valueSet = {
+              "",
+              "quickfix",
+              "refactor",
+              "refactor.extract",
+              "refactor.inline",
+              "refactor.rewrite",
+              "source",
+              "source.organizeImports",
+            },
+          },
+        },
+        isPreferredSupport = true,
+        disabledSupport = true,
+        dataSupport = true,
+        resolveSupport = {
+          properties = { "edit" },
+        },
+      }
+    '';
+
+    inlayHints = true;
+
+    keymaps = {
+      silent = true;
+      diagnostic = {
+        "<leader>k" = "goto_prev";
+        "<leader>j" = "goto_next";
+        "<leader>e" = "open_float";
+        "<leader>q" = "setloclist";
+      };
+      lspBuf = {
+        "gd" = "definition";
+        "gD" = "declaration";
+        "gi" = "implementation";
+        "gr" = "references";
+        "gt" = "type_definition";
+        "K" = "hover";
+        "<C-K>" = "signature_help";
+        "<leader>rn" = "rename";
+        "<leader>ca" = "code_action";
+        "<leader>f" = "format";
+      };
+    };
+
+    # set up buffer-local keymaps and features
+    onAttach = ''
+      -- enable inlay hints if supported
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+      end
+
+      -- enable document highlight if supported
+      if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          group = "lsp_document_highlight",
+          buffer = bufnr,
+          callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+          group = "lsp_document_highlight",
+          buffer = bufnr,
+          callback = vim.lsp.buf.clear_references,
+        })
+      end
+    '';
+  };
 }
--- home/common/required/nixvim/lsp/nix.nix
+++ home/common/required/nixvim/lsp/nix.nix
@@ -1,5 +1,31 @@
-_: {
-  programs.nixvim.plugins.nix = {
-    enable = true;
+{
+  pkgs,
+  ...
+}:
+{
+  programs.nixvim = {
+    plugins.nix = {
+      enable = true;
+    };
+
+    plugins.lsp.servers.nixd = {
+      enable = true;
+      settings = {
+        nixpkgs = {
+          expr = "import <nixpkgs> { }";
+        };
+        formatting = {
+          command = [ "${pkgs.nixfmt}" ];
+        };
+        options = {
+          nixos = {
+            expr = "(builtins.getFlake github:parthiv-krishna/nix-config).nixosConfigurations.*.options";
+          };
+          home-manager = {
+            expr = "(builtins.getFlake github:parthiv-krishna/nix-config).homeConfigurations.*.options";
+          };
+        };
+      };
+    };
   };
 }
