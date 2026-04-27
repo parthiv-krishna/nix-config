8a75a373a540741fa52308e0e08ff805e8f229ab
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2026-04-27 03:30:36 +0000

    caddy.nix: update plugins hash
    sha256-7DGnojZvcQBZ6LEjT0e5O9gZgsvEeHlQP9aKaJIs/Zg= -> sha256-VBmICI1wklu02jmgDRmmlfNc9ftK7a74uF280xzx8uc=

--- modules/features/selfhosted/base.nix
+++ modules/features/selfhosted/base.nix
@@ -116,7 +116,7 @@ lib.custom.mkFeature {
             enable = true;
             package = pkgs.caddy.withPlugins {
               plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-              hash = "sha256-7DGnojZvcQBZ6LEjT0e5O9gZgsvEeHlQP9aKaJIs/Zg=";
+              hash = "sha256-VBmICI1wklu02jmgDRmmlfNc9ftK7a74uF280xzx8uc=";
             };
             email = "letsencrypt.snowy015@passmail.net";
             acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
