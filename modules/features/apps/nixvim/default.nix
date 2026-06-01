1e5477af3e19e8957995e4b63f746c87ce1f93b9
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2026-06-01 15:48:25 -0700

    nixvim eval warning

--- modules/features/apps/nixvim/default.nix
+++ modules/features/apps/nixvim/default.nix
@@ -9,107 +9,111 @@ lib.custom.mkFeature {
 
   homeConfig =
     _cfg:
-    { config, ... }:
+    { config, pkgs, ... }:
     {
-      programs.nixvim.config = {
-        enable = true;
+      programs.nixvim = {
+        config = {
+          nixpkgs.source = pkgs.path;
 
-        viAlias = true;
-        vimAlias = true;
-        defaultEditor = true;
+          enable = true;
 
-        opts = {
-          # line numbers
-          number = true;
-          relativenumber = true;
+          viAlias = true;
+          vimAlias = true;
+          defaultEditor = true;
 
-          # search
-          hlsearch = true;
-          incsearch = true;
-          ignorecase = true;
-          showmatch = true;
-          smartcase = true;
+          opts = {
+            # line numbers
+            number = true;
+            relativenumber = true;
 
-          # whitespace
-          expandtab = true;
-          shiftwidth = 2;
-          tabstop = 2;
-          smartindent = true;
+            # search
+            hlsearch = true;
+            incsearch = true;
+            ignorecase = true;
+            showmatch = true;
+            smartcase = true;
 
-          # persistent undo
-          undofile = true;
+            # whitespace
+            expandtab = true;
+            shiftwidth = 2;
+            tabstop = 2;
+            smartindent = true;
 
-          # auto reload files changed on disk
-          autoread = true;
-        };
+            # persistent undo
+            undofile = true;
 
-        colorschemes = {
-          # Use base16 colorscheme with nix-colors
-          base16 = {
-            enable = true;
-            colorscheme = config.colorScheme.slug;
+            # auto reload files changed on disk
+            autoread = true;
           };
-        };
 
-        globals = {
-          mapleader = " ";
-        };
+          colorschemes = {
+            # Use base16 colorscheme with nix-colors
+            base16 = {
+              enable = true;
+              colorscheme = config.colorScheme.slug;
+            };
+          };
 
-        keymaps = [
-          # swap gj/j and gk/k
-          {
-            key = "j";
-            action = "gj";
-            mode = "n";
-          }
-          {
-            key = "k";
-            action = "gk";
-            mode = "n";
-          }
-          {
-            key = "gj";
-            action = "j";
-            mode = "n";
-          }
-          {
-            key = "gk";
-            action = "k";
-            mode = "n";
-          }
-          # swap g<down>/<down> and g<up>/<up>
-          {
-            key = "<down>";
-            action = "g<down>";
-          }
-          {
-            key = "<up>";
-            action = "g<up>";
-          }
-          {
-            key = "g<up>";
-            action = "<up>";
-          }
-          {
-            key = "g<down>";
-            action = "<down>";
-          }
-          # remove search highlight
-          {
-            key = "<Leader>n";
-            action = ":noh<CR>";
-          }
-          # vertical split
-          {
-            key = "<Leader>v";
-            action = ":vs<CR>";
-          }
-          # horizontal split
-          {
-            key = "<Leader>h";
-            action = ":sv<CR>";
-          }
-        ];
+          globals = {
+            mapleader = " ";
+          };
+
+          keymaps = [
+            # swap gj/j and gk/k
+            {
+              key = "j";
+              action = "gj";
+              mode = "n";
+            }
+            {
+              key = "k";
+              action = "gk";
+              mode = "n";
+            }
+            {
+              key = "gj";
+              action = "j";
+              mode = "n";
+            }
+            {
+              key = "gk";
+              action = "k";
+              mode = "n";
+            }
+            # swap g<down>/<down> and g<up>/<up>
+            {
+              key = "<down>";
+              action = "g<down>";
+            }
+            {
+              key = "<up>";
+              action = "g<up>";
+            }
+            {
+              key = "g<up>";
+              action = "<up>";
+            }
+            {
+              key = "g<down>";
+              action = "<down>";
+            }
+            # remove search highlight
+            {
+              key = "<Leader>n";
+              action = ":noh<CR>";
+            }
+            # vertical split
+            {
+              key = "<Leader>v";
+              action = ":vs<CR>";
+            }
+            # horizontal split
+            {
+              key = "<Leader>h";
+              action = ":sv<CR>";
+            }
+          ];
+        };
       };
     };
 }
