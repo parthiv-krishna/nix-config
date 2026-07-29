7914611d2273065981e00c078e7fced29d0f87e2
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2026-07-29 03:47:12 +0000

    factor out ci, rename tools/ to flake/

--- flake.nix
+++ flake.nix
@@ -188,44 +188,11 @@
         }
       );
 
-      herculesCI =
-        { primaryRepo, ... }:
-        let
-          pkgs = nixpkgs.legacyPackages.${systems.x86};
-          effects = import "${inputs.buildbot-nix}/herculesCI/effects-lib.nix" { inherit pkgs; };
-          knownHosts = pkgs.writeText "github-known-hosts" ''
-            github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
-          '';
-        in
-        {
-          onPush.default.outputs.effects.built-tag = effects.runIf (primaryRepo.branch or null == "main") (
-            effects.mkEffect {
-              name = "update-built-tag";
-              inputs = [
-                pkgs.git
-                pkgs.nss_wrapper
-                pkgs.openssh
-              ];
-              effectScript = ''
-                export USER=buildbot
-                export LOGNAME=buildbot
-                export NSS_WRAPPER_PASSWD="$HOME/passwd"
-                export NSS_WRAPPER_GROUP="$HOME/group"
-                export LD_PRELOAD="${pkgs.nss_wrapper}/lib/libnss_wrapper.so"
-                printf 'buildbot:x:0:0:Buildbot:%s:/bin/sh\n' "$HOME" > "$NSS_WRAPPER_PASSWD"
-                printf 'buildbot:x:0:\n' > "$NSS_WRAPPER_GROUP"
-                install -d -m 0700 "$HOME/.ssh"
-                install -m 0600 /run/secrets/buildbot-nix/github-nix-config-push-ssh-key "$HOME/.ssh/id_ed25519"
-                export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_ed25519 -o IdentitiesOnly=yes -o UserKnownHostsFile=${knownHosts}"
-
-                git init repository
-                git -C repository remote add origin git@github.com:parthiv-krishna/nix-config.git
-                git -C repository fetch --depth=1 origin ${lib.escapeShellArg primaryRepo.rev}
-                git -C repository push --force origin FETCH_HEAD:refs/tags/built
-              '';
-            }
-          );
-        };
+      herculesCI = import ./flake/ci.nix {
+        inherit lib;
+        inherit (inputs) buildbot-nix;
+        pkgs = nixpkgs.legacyPackages.${systems.x86};
+      };
 
       # Pre-commit
       checks = forEachSystem (
@@ -233,7 +200,7 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
-        import ./tools/checks.nix {
+        import ./flake/checks.nix {
           inherit inputs pkgs system;
           lib = customLib;
         }
@@ -245,7 +212,7 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
-        import ./tools/shell.nix {
+        import ./flake/shell.nix {
           inherit pkgs;
           checks = self.checks.${system};
         }
