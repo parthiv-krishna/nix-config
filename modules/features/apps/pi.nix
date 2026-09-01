f7bc829e96ab76358371429fac0703f57ea2d458
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2026-09-01 00:12:13 +0000

    pi-intercom

--- modules/features/apps/pi.nix
+++ modules/features/apps/pi.nix
@@ -33,6 +33,7 @@ lib.custom.mkFeature {
             "npm:@juicesharp/rpiv-ask-user-question@2.1.0"
             "npm:pi-background-tasks@2.4.2"
             "npm:@narumitw/pi-goal@0.28.0"
+            "npm:pi-intercom@0.12.1"
             "npm:pi-lens@3.8.71"
             "npm:@gotgenes/pi-permission-system@21.0.0"
             "npm:pi-subagents@0.35.1"
